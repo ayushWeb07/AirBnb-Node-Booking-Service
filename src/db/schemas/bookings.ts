21399d4e15ee57a2467ba44b4fd6a092c754214f
Ayush Bommana <bommanaayush07@gmail.com>
2026-04-27 13:44:14 +0530

    feat: Add idempotencyKey in bookings table

--- src/db/schemas/bookings.ts
+++ src/db/schemas/bookings.ts
@@ -1,4 +1,4 @@
-import { mysqlTable, int, mysqlEnum, timestamp } from "drizzle-orm/mysql-core"
+import { mysqlTable, int, mysqlEnum, timestamp, varchar } from "drizzle-orm/mysql-core"
 
 export const bookings = mysqlTable('bookings', {
     id: int("id").primaryKey().autoincrement(),
@@ -7,6 +7,7 @@ export const bookings = mysqlTable('bookings', {
     bookingAmount: int("bookingAmount").notNull(),
     totalGuests: int("totalGuests").notNull(),
     status: mysqlEnum("status", ["pending", "confirmed", "cancelled"]).notNull().default("pending"),
+    idempotencyKey: varchar("idempotencyKey", { length: 36 }).unique().notNull(),
     createdAt: timestamp("createdAt").notNull().defaultNow(),
     updatedAt: timestamp("updatedAt").notNull().defaultNow().onUpdateNow()
 });
